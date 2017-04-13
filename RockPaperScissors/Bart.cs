971db7b9bd2fd5ce05e4ed5c4d1619c81bc8e650
Pete Rossman <pete.rossman@gmail.com>
2017-04-13 08:52:24 -0400

    First commit

---
+++ RockPaperScissors/Bart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class Bart : IPlayer
+    {
+        public string getName()
+        {
+            return "Bart";
+        }
+
+        public Roshambo generateRoshambo()
+        {          
+            return Roshambo.Rock;
+        }
+    }
+}
---
+++ RockPaperScissors/Lisa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class Lisa : IPlayer
+    {
+        public string getName()
+        {
+            return "Lisa";
+        }
+
+        public Roshambo generateRoshambo()
+        {
+            
+
+            int play = 0;
+            Random rnd = new Random();
+            play = rnd.Next(1, 4);
+            switch (play)
+            {
+                case 1:
+                    return Roshambo.Paper;                   
+                case 2:
+                    return Roshambo.Rock;                   
+                case 3:
+                    return Roshambo.Scissors;                   
+                default:
+                    Console.WriteLine("Error!");
+                    break;
+            }
+            return Roshambo.Error;          
+        }
+    }
+}
